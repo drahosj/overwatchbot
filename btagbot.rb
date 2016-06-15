a82cd29c16a446ba187ae5f06ac6a9cd6191d1dd
Jake Drahos <jdrahos@cray.com>
2016-06-15 12:09:36 -0500

    Added better nicks

--- btagbot.rb
+++ btagbot.rb
@@ -3,11 +3,34 @@ require 'sqlite3'
 
 db = SQLite3::Database.new "overwatchbot.db"
 
+Heroes = [
+  "D.Va",
+  "Zarya",
+  "Bastion",
+  "Mei",
+  "Reaper",
+  "McCree",
+  "Hanzo",
+  "Widowmaker",
+  "Winston",
+  "Pharah",
+  "Genji",
+  "Mercy",
+  "Lucio",
+  "Junkrat",
+  "Roadhog",
+  "Symmetra",
+  "Tracer",
+  "Soldier76",
+  "Torbjorn",
+  "Reinhardt",
+  "Zenyatta"]
+
 bot = Cinch::Bot.new do
   configure do |c|
     c.server = "irc.us.cray.com"
     c.channels = ["#overwatch"]
-    c.nick = "OWBot"
+    c.nick = Heroes.sample
   end
 
   on :message, /^!btags/ do |m|
@@ -23,6 +46,24 @@ bot = Cinch::Bot.new do
     stmt = db.prepare "INSERT INTO battletags VALUES (?, ?);"
     stmt.execute m.user.nick, battletag
     m.channel.send("Nick: #{m.user.nick} changed battletag to #{battletag}")
+  end
+
+  on :message, /^we need to counter/ do |m|
+    m.bot.nick = Heroes.sample
+  end
+
+  on :message, /(.+) is a (scrub|noob|fail)/ do |m, hero|
+    if m.bot.nick.downcase == hero.downcase
+      m.bot.nick = Heroes.sample
+    end
+  end
+
+  on :message, /^we need a (.+)/ do |m, hero|
+    Heroes.each do |h|
+      if h.downcase == hero.downcase
+        m.bot.nick = h
+      end
+    end
   end
 end
 
